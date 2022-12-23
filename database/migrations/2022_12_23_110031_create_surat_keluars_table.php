d4881189074431dad61dfbb01377fe29f32a7fd2
fadhil <muhfadhilrachman@gmail.com>
2022-12-23 18:06:14 +0700

    databse

---
+++ database/migrations/2022_12_23_110031_create_surat_keluars_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratKeluarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat_keluars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no_agenda');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('jenis_surat');
+            $table->date('tanggal_kirim');
+            $table->integer('no_surat');
+            $table->string('pengirim');
+            $table->string('perihal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surat_keluars');
+    }
+}
---
+++ database/migrations/2022_12_23_110044_create_surat_masuks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratMasuksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat_masuks', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            $table->integer('no_agenda');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('jenis_surat');
+            $table->date('tanggal_kirim');
+            $table->integer('no_surat');
+            $table->string('pengirim');
+            $table->string('perihal');
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surat_masuks');
+    }
+}
